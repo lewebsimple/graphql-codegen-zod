5fbe590dace14500bb925f508b9fc70a9fae384d
Pascal Martineau <pascal@lewebsimple.ca>
2026-03-06 09:32:06 -0500

    feat: Configure typescript / typescript-operations / typed-document-node plugins

--- src/preset.ts
+++ src/preset.ts
@@ -1,8 +1,78 @@
+import { basename, dirname, join } from "node:path";
+
+import * as addPlugin from "@graphql-codegen/add";
+import * as typedDocumentNodePlugin from "@graphql-codegen/typed-document-node";
+import * as typescriptOperationsPlugin from "@graphql-codegen/typescript-operations";
+import * as typescriptPlugin from "@graphql-codegen/typescript";
+import * as zodPlugin from "./plugin";
 import type { Types } from "@graphql-codegen/plugin-helpers";
 
 export const preset: Types.OutputPreset = {
-  buildGeneratesSection: async (_options) => {
-    const sections: Types.GenerateOptions[] = [];
+  buildGeneratesSection: async (options) => {
+    const baseConfig = {
+      config: {
+        ...options.config,
+        avoidOptionals: {
+          defaultValue: false,
+          field: true,
+          inputValue: false,
+          object: true,
+        },
+        useTypeImports: true,
+      },
+      documents: options.documents,
+      schema: options.schema,
+      schemaAst: options.schemaAst,
+      pluginMap: {
+        ...options.pluginMap,
+        add: addPlugin,
+        typescript: typescriptPlugin,
+        typescriptOperations: typescriptOperationsPlugin,
+        typedDocumentNode: typedDocumentNodePlugin,
+        zod: zodPlugin,
+      },
+    } satisfies Partial<Types.GenerateOptions>;
+
+    const outputDir = dirname(options.baseOutputDir);
+    const outputName = basename(options.baseOutputDir);
+
+    const sections: Types.GenerateOptions[] = [
+      // typescript / typescript-operations
+      {
+        ...baseConfig,
+        filename: join(outputDir, "types.ts"),
+        plugins: [
+          {
+            typescript: {},
+          },
+          {
+            typescriptOperations: {},
+          },
+        ],
+      },
+
+      // typed-document-node
+      {
+        ...baseConfig,
+        filename: join(outputDir, "documents.ts"),
+        plugins: [
+          { add: { content: `import type * as Types from "./types";\n` } },
+          {
+            typedDocumentNode: {
+              importOperationTypesFrom: "./types",
+            },
+          },
+        ],
+      },
+
+      // zod
+      {
+        ...baseConfig,
+        filename: join(outputDir, outputName),
+        plugins: [{ zod: {} }],
+      },
+    ];
+
     return sections;
   },
 };
